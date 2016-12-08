3cf0db661f4bd549862aa7f1f603e6dd072901fc
root <root@packer-debian-8-amd64.droplet.local>
2016-12-08 01:28:46 +0000

    added admin to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,21 @@ end
 
 f= Faculty.create( :firstName => "Transfer", :lastName => "Credit", :sectionNumber => "0")
 
-a= CourseDescription.create(number: 1, name: "Transfer", category: "N/A", hours: "1", department: "TRANSFER")
-b= CourseDescription.create(number: 2, name: "Transfer", category: "N/A", hours: "2", department: "TRANSFER")
-c= CourseDescription.create(number: 3, name: "Transfer", category: "N/A", hours: "3", department: "TRANSFER")
-d= CourseDescription.create(number: 4, name: "Transfer", category: "N/A", hours: "4", department: "TRANSFER")
+a= CourseDescription.create(number: 1, name: "Transfer 1 Credit", category: "N/A", hours: "1", department: "TRANSFER")
+b= CourseDescription.create(number: 2, name: "Transfer 2 Credits", category: "N/A", hours: "2", department: "TRANSFER")
+c= CourseDescription.create(number: 3, name: "Transfer 3 Credits", category: "N/A", hours: "3", department: "TRANSFER")
+d= CourseDescription.create(number: 4, name: "Transfer 4 Credits", category: "N/A", hours: "4", department: "TRANSFER")
 
 
 year= 2006
 14.times do |x|
-
-
-
   Semester.create(course_description_id: a.id, faculty_id: f.id, year: year, semester: "N/A" )
   Semester.create(course_description_id: b.id, faculty_id: f.id, year: year, semester: "N/A" )
   Semester.create(course_description_id: c.id, faculty_id: f.id, year: year, semester: "N/A" )
   Semester.create(course_description_id: d.id, faculty_id: f.id, year: year, semester: "N/A" )
   year= year+1
 end
+
+Admin.create(:firstName => "Transfer", :lastName => "Credit", :PID => 987654321)
+
+Faculty.first.update(:PID=>111122223)
